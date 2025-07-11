ca244a7349000ef3357c71547ddd44d178651c38
One Object <haxttanak@gmail.com>
2025-07-12 02:01:05 +0300

    first commit

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -10,6 +10,12 @@ import (
 )
 
 func Init(cfg *config.Config) *gorm.DB {
+	// DEBUG: выводим значения полей конфигурации БД
+	fmt.Println("DEBUG DBHost:", cfg.DBHost)
+	fmt.Println("DEBUG DBUser:", cfg.DBUser)
+	fmt.Println("DEBUG DBPassword:", cfg.DBPassword)
+	fmt.Println("DEBUG DBName:", cfg.DBName)
+	fmt.Println("DEBUG DBPort:", cfg.DBPort)
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
 		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
