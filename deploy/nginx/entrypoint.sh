9f0b5c5ee51e1585718c0fbb014116b6f6cc41c7
One Object <haxttanak@gmail.com>
2025-07-12 04:52:55 +0300

    16

--- deploy/nginx/entrypoint.sh
+++ deploy/nginx/entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 set -e
 
-# Use sed to replace the domain name in the template.
-# This is more robust than envsubst as it doesn't require extra packages
-# and won't accidentally replace other shell variables.
-cat /etc/nginx/templates/default.conf.template | sed "s/\${DOMAIN_NAME}/${DOMAIN_NAME}/g" > /etc/nginx/conf.d/default.conf
+echo "[DEBUG] Entrypoint script started."
 
+echo "[DEBUG] Available environment variables:"
+printenv
+echo "------------------------------------"
+
+TEMPLATE_PATH="/etc/nginx/templates/default.conf.template"
+CONFIG_PATH="/etc/nginx/conf.d/default.conf"
+
+echo "[DEBUG] Generating config from $TEMPLATE_PATH to $CONFIG_PATH..."
+
+# Replace variable and create the final config file
+cat "${TEMPLATE_PATH}" | sed "s/\${DOMAIN_NAME}/${DOMAIN_NAME}/g" > "${CONFIG_PATH}"
+
+echo "[DEBUG] Config file generated. Content of $CONFIG_PATH:"
+echo "------------------------------------"
+cat "${CONFIG_PATH}"
+echo "------------------------------------"
+
+echo "[DEBUG] Starting Nginx..."
 # Execute the command passed to this script (e.g., nginx -g 'daemon off;')
 exec "$@"
