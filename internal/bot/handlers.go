e0e42fd11485ff578753b9c3fac39a3129b2974c
One Object <haxttanak@gmail.com>
2025-07-13 10:27:21 +0300

    16

--- internal/bot/handlers.go
+++ internal/bot/handlers.go
@@ -2,9 +2,9 @@ package bot
 
 import (
 	"log"
-	"strconv"
-	"gorm.io/gorm"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gorm.io/gorm"
 )
 
 // SetupWebhook настраивает webhook для Telegram бота
@@ -36,7 +36,7 @@ func ProcessUpdate(update tgbotapi.Update, bot *tgbotapi.BotAPI, db *gorm.DB) {
 	if update.Message != nil {
 		handleMessage(update.Message, bot, db)
 	}
-	
+
 	// Handle other types of updates (callback queries, etc.)
 	if update.CallbackQuery != nil {
 		handleCallbackQuery(update.CallbackQuery, bot, db)
@@ -44,49 +44,41 @@ func ProcessUpdate(update tgbotapi.Update, bot *tgbotapi.BotAPI, db *gorm.DB) {
 }
 
 func handleMessage(message *tgbotapi.Message, bot *tgbotapi.BotAPI, db *gorm.DB) {
-	// Save user to database
+	// Ignore messages without a sender
+	if message.From == nil {
+		return
+	}
+
+	// Save user and message to the database
 	saveUser(message.From, db)
-	
-	// Save message to database
 	saveMessage(message, db)
-	
+
 	// Handle commands
 	if message.IsCommand() {
 		handleCommand(message, bot, db)
 		return
 	}
-	
+
 	// Handle regular messages
-	userInfo := ""
-	if message.From != nil {
-		userInfo = "[ID: " + strconv.FormatInt(int64(message.From.ID), 10) + ", Имя: " + message.From.FirstName
-		if message.From.LastName != "" {
-			userInfo += " " + message.From.LastName
-		}
-		userInfo += "] "
-	}
-	msg := tgbotapi.NewMessage(message.Chat.ID, userInfo+"Привет! Я бот с webhook.")
+	log.Printf("Message from %s: %s", formatUserInfo(message.From), message.Text)
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Ваше сообщение получено: "+message.Text)
 	bot.Send(msg)
 }
 
 func handleCommand(message *tgbotapi.Message, bot *tgbotapi.BotAPI, db *gorm.DB) {
-	userInfo := ""
-	if message.From != nil {
-		userInfo = "[ID: " + strconv.FormatInt(int64(message.From.ID), 10) + ", Имя: " + message.From.FirstName
-		if message.From.LastName != "" {
-			userInfo += " " + message.From.LastName
-		}
-		userInfo += "] "
-	}
+	userInfo := formatUserInfo(message.From)
+
 	switch message.Command() {
 	case "start":
-		msg := tgbotapi.NewMessage(message.Chat.ID, userInfo+"Привет! Я бот. Используй /help для получения справки.")
+		// Corrected greeting message
+		msgText := "Привет, " + userInfo + ", рады видеть вас снова!"
+		msg := tgbotapi.NewMessage(message.Chat.ID, msgText)
 		bot.Send(msg)
 	case "help":
-		msg := tgbotapi.NewMessage(message.Chat.ID, userInfo+"Доступные команды:\n/start - Начать работу с ботом\n/help - Показать справку")
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Доступные команды:\n/start - Начать работу с ботом\n/help - Показать справку")
 		bot.Send(msg)
 	default:
-		msg := tgbotapi.NewMessage(message.Chat.ID, userInfo+"Неизвестная команда. Используй /help для получения справки.")
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Неизвестная команда. Используй /help для получения справки.")
 		bot.Send(msg)
 	}
 }
@@ -95,6 +87,9 @@ func handleCallbackQuery(callback *tgbotapi.CallbackQuery, bot *tgbotapi.BotAPI,
 	// Handle callback queries
 	callbackConfig := tgbotapi.NewCallback(callback.ID, "Callback received!")
 	bot.Request(callbackConfig)
+
+	// You might want to log the callback data
+	log.Printf("Callback from %s: %s", callback.From.UserName, callback.Data)
 }
 
 func saveUser(user *tgbotapi.User, db *gorm.DB) {
@@ -107,4 +102,16 @@ func saveMessage(message *tgbotapi.Message, db *gorm.DB) {
 	// Implementation for saving message to database
 	// This is a placeholder - implement based on your database schema
 	log.Printf("Message from %d: %s", message.From.ID, message.Text)
-} 
+}
+
+// formatUserInfo creates a user-friendly string from a User object.
+func formatUserInfo(user *tgbotapi.User) string {
+	if user == nil {
+		return "Anonymous"
+	}
+	name := user.FirstName
+	if user.LastName != "" {
+		name += " " + user.LastName
+	}
+	return name
+}
--- internal/database/models.go
+++ internal/database/models.go
@@ -6,32 +6,32 @@ import (
 
 // User represents a Telegram user
 type User struct {
-	ID         uint64    `gorm:"primaryKey"`
-	TelegramID int64     `gorm:"uniqueIndex:idx_users_telegram_id;not null"`
-	Username   string    `gorm:"size:255"`
-	FirstName  string    `gorm:"size:255"`
-	LastName   string    `gorm:"size:255"`
+	ID         uint64 `gorm:"primaryKey"`
+	TelegramID int64  `gorm:"uniqueIndex:idx_users_telegram_id;not null"`
+	Username   string `gorm:"size:255"`
+	FirstName  string `gorm:"size:255"`
+	LastName   string `gorm:"size:255"`
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
 
 // Message represents a user message
 type Message struct {
-	ID          uint64    `gorm:"primaryKey"`
-	UserID      uint64    `gorm:"index"`
-	MessageText string    `gorm:"type:text"`
-	MessageType string    `gorm:"size:50"`
+	ID          uint64 `gorm:"primaryKey"`
+	UserID      uint64 `gorm:"index"`
+	MessageText string `gorm:"type:text"`
+	MessageType string `gorm:"size:50"`
 	CreatedAt   time.Time
 }
 
 // Admin represents an administrator with security features
 type Admin struct {
-	ID                  uint64    `gorm:"primaryKey"`
-		Login               string    `gorm:"type:citext;uniqueIndex;not null"`
-		HashedPassword      string    `gorm:"size:255;not null"`
-	IsActive            bool      `gorm:"default:true"`
+	ID                  uint64 `gorm:"primaryKey"`
+	Login               string `gorm:"type:citext;uniqueIndex;not null"`
+	HashedPassword      string `gorm:"size:255;not null"`
+	IsActive            bool   `gorm:"default:true"`
 	LastLoginAt         *time.Time
-	FailedLoginAttempts int       `gorm:"default:0"`
+	FailedLoginAttempts int `gorm:"default:0"`
 	LockedUntil         *time.Time
 	CreatedAt           time.Time
 	UpdatedAt           time.Time
@@ -40,4 +40,4 @@ type Admin struct {
 // TableName specifies the table name for Admin
 func (Admin) TableName() string {
 	return "admins"
-} 
+}
